9ce28ed952743da983b2a508342256c1dda3930c
lby132 <quddbscjswow@naver.com>
2023-03-13 00:06:39 +0900

    jpql

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -26,24 +26,43 @@ public class JpaMain {
             final Member member1 = new Member();
             member1.setUsername("회원1");
             member1.setTeam(teamA);
+            member1.setType(ADMIN);
             em.persist(member1);
 
             final Member member2 = new Member();
             member2.setUsername("회원2");
             member2.setTeam(teamA);
+            member2.setType(ADMIN);
             em.persist(member2);
 
             final Member member3 = new Member();
             member3.setUsername("회원3");
             member3.setTeam(teamB);
+            member3.setType(ADMIN);
             em.persist(member3);
 
-            final int resultCount = em.createQuery("update Member m set m.age = 20")
-                    .executeUpdate();
+            // 기본 CASE 식
+//            String query =
+//                    "select " +
+//                            "case when m.age <= 10 then '학생요금'" +
+//                            "     when m.age <= 60 then '경로요금'" +
+//                            "     else '일반요금'" +
+//                            "end " +
+//                    "from Member m";
 
-            System.out.println("resultCount = " + resultCount);
+            // CASE식 (사용자 이름이 없으면 이름 없는 회원을 반환함)
+//            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
 
-            System.out.println("member1.getAge() = " + member1.getAge());
+            // 사용자 이름이 관리자면 null을 반환하고 나머지는 본인의 이름을 반환함
+            String query = "select nullif(m.username, '관리자') from Member m";
+
+
+            List<String> result = em.createQuery(query, String.class).getResultList();
+
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
+
 
             tx.commit();
         } catch (Exception e) {
